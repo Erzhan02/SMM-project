9f2b558eb25f2824879d46889c6c678085b5d7f4
erzhan <erjik1902@gmail.com>
2025-05-06 19:00:29 +0600

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import Inspect from 'vite-plugin-inspect';
 
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), Inspect()],
